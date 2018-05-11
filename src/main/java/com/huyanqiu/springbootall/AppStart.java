bff656e10d6975d34192616bfc7230ab332eac83
huyanqiu <hyq@blueocean-health.com>
2018-05-11 14:56:51 +0800

    支持注解事务、启动定时任务

--- src/main/java/com/huyanqiu/springbootall/AppStart.java
+++ src/main/java/com/huyanqiu/springbootall/AppStart.java
@@ -2,10 +2,13 @@ package com.huyanqiu.springbootall;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication //申明让spring boot自动给程序进行必要的配置
+@ServletComponentScan   //开启后支持自定义Servlet和Filter
+@EnableScheduling       //支持定时任务
 @MapperScan(basePackages={"com.huyanqiu.springbootall.mapper"}) // 扫描mapper的java文件，否则每个Mapper文件上都需要加 @Mapper注解
 /**
  * DataSourceAutoConfiguration类使用了@Configuration注解向spring注入了dataSource bean。
